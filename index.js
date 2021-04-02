9fcae0e0107059e1d0fb8b253544b56b7260b45b
Nishan <arifuzzamannishan@gmail.com>
2021-04-02 17:52:27 +0600

    comment added

--- index.js
+++ index.js
@@ -13,19 +13,20 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.furzf.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
-// console.log(process.env.DB_NAME)
 
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 client.connect(err => {
+    // for new data insert
     const newMobileCollection = client.db("goMobile").collection("addNewMobileInfo");
     console.log("database connected");
 
+    // for order data
     const orderCollection = client.db("goMobile").collection("order");
     console.log("database connected");
 
-
+    // for insert api
     app.post('/addNewMobileInfo', (req, res) => {
         const newMobileInfo = req.body;
         console.log('adding new mobile: ', newMobileInfo);
@@ -36,6 +37,7 @@ client.connect(err => {
             })
     })
 
+    // for order api
     app.post('/order', (req, res) => {
         const newOrder = req.body;
         orderCollection.insertOne(newOrder)
@@ -45,32 +47,32 @@ client.connect(err => {
             })
     })
 
+    // for specific data find api
     app.get('/product/:id', (req, res) => {
         newMobileCollection.find({ _id: ObjectID(req.params.id) })
             .toArray((err, documents) => {
                 res.send(documents[0])
-                // console.log(documents);
             })
     })
 
+    // for order show api
     app.get('/showOrder', (req, res) => {
         console.log(req.query.email);
         orderCollection.find({email: req.query.email})
             .toArray((err, documents) => {
                 res.send(documents)
-                // console.log(documents);
             })
     })
 
+    // for read all data
     app.get('/mobileInfo', (req, res) => {
         newMobileCollection.find()
             .toArray((err, items) => {
-                // console.log('from database', items);
-                // console.log(err);
                 res.send(items);
             })
     })
 
+    // for delete api
     app.delete('/delete/:id',(req,res) => {
         newMobileCollection.deleteOne({_id: ObjectID(req.params.id)})
         .then(result => {
@@ -78,20 +80,7 @@ client.connect(err => {
         })
     })
 
-
-
-
-
-
-
 });
 
-
-
-
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
 
 app.listen(port || process.env.PORT);
